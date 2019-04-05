aefcc92152ca6e85d8eeca6b0660dac7dd4ed0f2
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-06 04:58:11 +0500

    Update mock

--- mock/index.ts
+++ mock/index.ts
@@ -3,7 +3,45 @@ import mockjs from 'mockjs'
 
 export default {
     'POST /auth/page/prizm-json-plugin/login': mockjs.mock({
-        'status': 'fail',
-        'message': 'Неверное сочетание имени пользователя и пароля'
+        'status': 'ok',
+        'data': {
+            'ident': 'geraldus',
+            'id': 1
+        }
+    }),
+    'POST api/user/read/12': mockjs.mock({
+        'email': 'heraldhoi@gmail.com',
+        'accessRights': [
+            'ConfirmDeposit',
+            'ConfirmWithdrawal',
+            'RejectDeposit',
+            'RejectWithdrawal',
+            'MakeDeposit',
+            'RequestWithdrawal',
+            'SelfBalanceView',
+            'SelfBalanceHistory',
+            'SelfListDeposit',
+            'SelfListWithdrawal',
+            'SelfDepositCancel',
+            'SelfWithdrawalCancel',
+            'ListWithdrawal',
+            'ListDeposit',
+            'DeleteCurrency',
+            'UpdateCurrency',
+            'CreateCurrency',
+            'ListCurrency',
+            'DeleteUser',
+            'UpdateUser',
+            'ViewUser',
+            'CreateUser',
+            'ListUsers'
+        ],
+        'lastName': 'Файзрахманов',
+        'patronymic': 'Сахиевич',
+        'verified': true,
+        'ident': 'geraldus',
+        'firstName': 'Артур',
+        'id': 12
     })
+
 }
