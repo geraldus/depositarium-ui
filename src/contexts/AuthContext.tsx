9f14d600eacea2cf4123e5301ad6c5b15d62bbdb
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-05 21:38:08 +0500

    Provide better typed definition of Auth Context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -11,24 +11,59 @@ export interface UserContext {
     user?: UserData
 }
 
+export interface SigninFormContext {
+    login(u: UserContext): void,
+    logout(): void
+}
+
+type Context = UserContext & SigninFormContext
+
 declare global {
     interface Window {
         currentUser: UserContext
     }
 }
 
-const defaultState: UserContext = {
+const defaultState: Context = {
     auth: false,
-    user: undefined
+    user: undefined,
+    login: () => {},
+    logout: () => {}
 }
 
-const AuthContext = React.createContext(window.currentUser)
+const AuthContext = React.createContext({
+    ...defaultState,
+    ...window.currentUser
+})
 
-class AuthProvider extends React.Component<{}, UserContext> {
+console.log('!CTX!', window, window.currentUser)
+
+class AuthProvider extends React.Component<{}, Context> {
     readonly state = defaultState
+    componentDidMount() {
+        if (window.currentUser && window.currentUser.auth) {
+            this.setState(window.currentUser)
+        }
+    }
+    login = (creds: UserContext) => {
+        console.log('UPDATE CONTEXT', creds)
+        this.setState({
+            ...creds,
+            auth: true
+        })
+    }
+    logout = () => {
+        this.setState(defaultState)
+    }
     render () {
+        console.log('CTX', window, window.currentUser)
         return (
-            <AuthContext.Provider value={ this.state }>
+            <AuthContext.Provider
+                    value={{
+                        ...this.state,
+                        login: this.login.bind(this),
+                        logout: this.logout
+                    }}>
                 {this.props.children}
             </AuthContext.Provider>
         )
