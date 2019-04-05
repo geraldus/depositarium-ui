9509f7b9d53fa96530bd9aa106164b66c142d553
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-05 16:21:23 +0500

    Implement default menu

---
+++ src/containers/defaultMenu.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Menu } from 'antd'
+import { formatMessage } from 'umi-plugin-locale'
+
+
+// export const defaultMenu
+// <Menu defaultSelectedKeys={['1']} mode='horizontal' theme='dark' >
+//                     <Menu.Item style={{float: 'left'}}>
+//                         <NavLink to='/'>
+//                             {formatMessage({id: 'pageTitle.home'})}
+//                         </NavLink>
+//                     </Menu.Item>
+//                 {/* {c && c.user && items.account.items.length > 0 &&
+//                     <Menu.Item style={{float: 'right'}}>
+//                         <Dropdown overlay={this.accountMenu()} trigger={['click']}>
+//                             <a className="ant-dropdown-link" href="#">{items.account.user.fullName}</a>
+//                         </Dropdown>
+//                     </Menu.Item>
+//                 } */}
+// </Menu>
+
+// export const defaultItems = (<>
+//     <Menu.Item style={{float: 'left'}}>
+//         <NavLink to='/'>
+//             {formatMessage({id: 'pageTitle.home'})}
+//         </NavLink>
+//     </Menu.Item>
+// </>)
