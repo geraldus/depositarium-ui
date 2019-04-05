013a64f54aa1e09a92552c2faf4d9e1b4a08026c
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-05 16:24:06 +0500

    Add messages

--- src/locales/ru-RU.ts
+++ src/locales/ru-RU.ts
@@ -1,6 +1,19 @@
 export default {
-  'index.start': 'Getting Started',
-  'general.deposit': 'Пополнение',
-  'general.withdrawal': 'Вывод',
-  'general.startDate': 'Дата отсчёта'
+    'index.start': 'Getting Started',
+    'wallet.operations': 'Операции',
+    'pageTitle.home': 'Главная',
+    'pageTitle.signin': 'Вход',
+    'general.cancel': 'Отмена',
+    'general.signin': 'Войти',
+    'general.depositIn': 'Ввод',
+    'general.deposit': 'Пополнение',
+    'general.withdrawal': 'Вывод',
+    'general.history': 'История',
+    'general.startDate': 'Дата отсчёта',
+    'general.nextAccountingIn': 'Следующая фиксация',
+    'general.programOperations': 'Операции по программе',
+    'form.error.usernameRequired': 'Укажите логин для входа',
+    'form.error.passwordRequired': 'Введите пароль',
+    'form.placeholder.username': 'Эл.адрес или логин',
+    'form.placeholder.password': 'Пароль'
 }
