73837837d1ebca3353cb67c767ef1a8ec7bc96d4
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-05 16:26:34 +0500

    Dummy index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import styles from './index.css';
 import { formatMessage } from 'umi-plugin-locale';
+import Title from 'antd/lib/typography/Title';
+
+import withRouter from 'umi/withRouter';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 
 export default function() {
   return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            {formatMessage({ id: 'index.start' })}
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <Title>{formatMessage({id: 'pageTitle.home'})}</Title>
+  )
 }
+
