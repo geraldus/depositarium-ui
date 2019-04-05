a732c1c1e271e88ad4f957cf84b4cab34bbfdecb
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-05 16:25:54 +0500

    First (broken) implementation of CSS route transition

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-// import styles from './index.css';
+import React, { Children } from 'react';
+import { AppNav } from '@/containers/app-nav';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
 
-const BasicLayout: React.FC = props => {
-  return (
-    <div>
-      {props.children}
-    </div>
-  );
-};
+import withRouter from 'umi/withRouter';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { AuthProvider } from '@/contexts/AuthContext'
 
-export default BasicLayout;
+
+const {
+    Header, Content, Footer, Sider,
+  } = Layout
+
+type Props = {
+    location: typeof window.location,
+}
+
+
+class BasicLayout extends React.Component<Props> {
+    render () {
+        const { props } = this
+        return (
+            <AuthProvider>
+                <Layout>
+                    <Header>
+                        <AppNav/>
+                    </Header>
+                    {props.children}
+                </Layout>
+            </AuthProvider>
+          )
+    }
+}
+
+export default withRouter(({ location, children }) => <TransitionGroup>
+    <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+        <BasicLayout location={location}>
+            { children }
+        </BasicLayout>
+    </CSSTransition>
+</TransitionGroup>);
