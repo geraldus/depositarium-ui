9c86e9afe6849f7429350e429bffa802050edae4
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-06 04:58:57 +0500

    Dummy index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Typography } from 'antd'
+import { Typography, Row, Col } from 'antd'
 const { Title, Paragraph } = Typography
 
 import { formatMessage } from 'umi-plugin-locale'
 
 import styles from './index.css'
 
-
-export default function() {
-  return (<>
-        <Title>{formatMessage({id: 'pageTitle.home'})}</Title>
-        <Paragraph>
-            Мы ещё не решили, что будет на главной странице.
-        </Paragraph>
-    </>)
+export default () => {
+  return (
+        <Row type='flex' justify='center'>
+            <Col xs={20}>
+                <Title>{formatMessage({id: 'pageTitle.home'})}</Title>
+                <Paragraph>
+                    Мы ещё не решили, что будет на главной странице.
+                </Paragraph>
+            </Col>
+        </Row>
+    )
 }
