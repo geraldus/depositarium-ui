c8c1410b01f0fc12741059057d2b61f7ad21767d
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-07 22:56:42 +0500

    Connect index layout to DVA

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,7 +2,7 @@ import React, { createRef, ComponentPropsWithRef, ComponentProps } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import { Modal, Layout } from 'antd'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
-import { AppNav } from '@/containers/app-nav';
+import AppNav from '@/containers/app-nav';
 import { AuthProvider, UserContext } from '@/contexts/AuthContext'
 import { AuthConsumer, AuthContext } from '@/contexts/AuthContext'
 import { WrappedSigninForm, SigninForm } from '@/containers/form/signin'
@@ -10,34 +10,59 @@ import { WrappedSigninForm, SigninForm } from '@/containers/form/signin'
 import withRouter from 'umi/withRouter';
 import _ from 'lodash'
 import { formatMessage } from 'umi-plugin-locale'
+import { connect } from 'dva'
 
 import styles from 'antd/dist/antd.css'
+import { stat } from 'fs';
 
 const { Header, Content, Footer } = Layout
 
 
-type Props = RouteComponentProps & ComponentPropsWithRef<any>
-
-type FormRef = React.RefObject<SigninForm> & React.ComponentProps
-
+const defaultProps = {
+    user: {
+        accessRights: [],
+        signinFormVisible: false
+    },
+    dispatch: () => {},
+}
 const initialState = {
     signinForm: false
 }
+
+type Props =
+    typeof defaultProps
+    & RouteComponentProps
+    & ComponentPropsWithRef<any>
+type FormRef = React.RefObject<SigninForm> & React.ComponentPropsWithoutRef<SigninForm>
 type State = typeof initialState
 
 class BasicLayout extends React.Component<Props, State> {
     static contextType = AuthContext
+    static readonly defaultProps = defaultProps
     readonly state = initialState
     private signinFormRef = createRef<FormRef>()
+    componentDidMount () {
+        this.props.dispatch({ type: 'user/info' })
+    }
     saveSigninFormRef (formRef: FormRef) {
         this.signinFormRef = formRef
     }
     signinFormShow (e: React.MouseEvent<HTMLElement>) {
         e.preventDefault()
-        this.setState({ signinForm: true })
+        this.props.dispatch({
+            type: 'user/updateState',
+            payload: {
+                signinFormVisible: true
+            }
+        })
     }
     handleSigninFormClose () {
-        this.setState({ signinForm: false })
+        this.props.dispatch({
+            type: 'user/updateState',
+            payload: {
+                signinFormVisible: false
+            }
+        })
     }
     handleAuthResult (r, login: (e: UserContext) => void) {
         const form = this.signinFormRef.props.form
@@ -56,46 +81,45 @@ class BasicLayout extends React.Component<Props, State> {
         }
     }
     signin (e: React.FormEvent, login: (e: UserContext) => void) {
+        const { dispatch } = this.props
         const form = this.signinFormRef.props.form
         form.validateFields((err, values) => {
             if (!err) {
-                const formData = new FormData()
-                formData.append('username', values['username'])
-                formData.append('password', values['password'])
-                fetch('/auth/page/prizm-json-plugin/login?_accept=application/json', {
-                    method: 'POST',
-                    body: formData
+                dispatch({
+                    type: 'user/signin',
+                    payload: values
                 })
-                    .then(r => r.json())
-                    .then(j => {
-                        this.handleAuthResult(j, login)
-                    })
-                    .catch(e => console.log(e))
             }
         })
     }
     render () {
         const { props } = this
+        const { location } = props
+        const formVisible = this.props.user.signinFormVisible
         return (
-            <Layout>
                 <AuthConsumer>
                     {({ login, logout, ...auth }) => {
                         const handleOk = (e) => {
                             this.signin(e, login)
                         }
-                        return(<>
+                        return(<Layout>
                         <Header>
                             <AppNav
-                                { ...auth }
+                                { ...this.props.user }
                                 handleSigninClick={e => this.signinFormShow(e)}
                                 />
                         </Header>
-                        <Content>
-                            {props.children}
-                        </Content>
+                        <TransitionGroup>
+                            <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+                                <Content>
+                                    {props.children}
+                                </Content>
+                            </CSSTransition>
+                        </TransitionGroup>
+                        <pre>{JSON.stringify(this.props.user, null, 2)}</pre>
                         <Modal
                                 title={formatMessage({id: 'pageTitle.signin'})}
-                                visible={this.state.signinForm}
+                                visible={formVisible}
                                 centered
                                 destroyOnClose={true}
                                 okText={formatMessage({id: 'general.signin'})}
@@ -103,25 +127,40 @@ class BasicLayout extends React.Component<Props, State> {
                                 onOk={e => handleOk(e)}
                                 onCancel={e => this.handleSigninFormClose()} >
                             <WrappedSigninForm
+                                formErrors={this.props.formErrors}
                                 wrappedComponentRef={(ref: FormRef) => this.saveSigninFormRef(ref)} />
                         </Modal>
                         <Footer>© 2019 Internation Operator Association</Footer>
-                    </>)}
+                    </Layout>)}
                 }
                 </AuthConsumer>
-            </Layout>
           )
     }
 }
 
-export default withRouter(({ location, children }) =>
+const mapStateToProps = (state) => {
+    return ({
+        user: state.user,
+        signinFormVisible: state.user.signinFormVisible
+    })
+}
+const mapDispatchToProps = (dispatch) => {
+    return ({
+        dispatch: dispatch
+    })
+}
+
+
+const AuthProvidedLayout = ({ location, children, dispatch, user }) => (
     <AuthProvider>
-        <TransitionGroup>
-            <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
-                <BasicLayout location={location}>
-                    { children }
-                </BasicLayout>
-            </CSSTransition>
-        </TransitionGroup>
+        <BasicLayout location={location} dispatch={dispatch} user={user}>
+            { children }
+        </BasicLayout>
     </AuthProvider>
 )
+
+const ConnectedLayout = connect(mapStateToProps, mapDispatchToProps)(AuthProvidedLayout)
+
+const RoutedLayout = withRouter(ConnectedLayout)
+
+export default RoutedLayout
