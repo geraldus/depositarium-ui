1f61204c012e7dbf99dc9af8abb1bb06afbef1b2
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-04-09 07:43:03 +0500

    Add and generalize utils

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,11 +1,19 @@
 import { UserData } from '@/contexts/AuthContext';
 
-export const mkIdent = (m: UserData): string => {
+export const mkFullName = (m: UserData): string | null => {
     const { firstName, lastName, patronymic } = m
     let fullName = []
     lastName != '' ? fullName.push(lastName) : null
     firstName != '' ? fullName.push(firstName) : null
     patronymic != '' ? fullName.push(patronymic) : null
     const name = fullName.join(' ')
-    return name == '' ? m.ident : name
+    return name == '' ? null : name
 }
+
+
+export const mkIdent = (u: UserData): string => {
+    const name = mkFullName(u) || ''
+    return name == '' ? u.ident : name
+}
+
+
